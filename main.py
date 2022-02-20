3d3c1a4e12beae1dce268153b199ecd90f681972
python <python290821@gmail.com>
2022-02-20 19:11:44 +0200

    upload

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # localhost:5000/
 @app.route("/")
 def home():
-    return '''
-    <!DOCTYPE html>
-<html>
-<body>
-
-<canvas id="myCanvas" width="200" height="100" style="border:1px solid #d3d3d3;">
-Your browser does not support the HTML canvas tag.</canvas>
-
-<script>
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.font = "30px Arial";
-ctx.strokeText("Hello World",10,50);
-</script>
-
-</body>
-</html>'''
+    return render_template('home.html')
 
 app.run()
 
